236435f25c22487367d61f001b61ed4552135aee
ZpVictor <madscida9@gmail.com>
2025-08-07 16:11:41 -0300

    Tabauda

--- Tabuada/Main.py
+++ Tabuada/Main.py
@@ -1,5 +1,41 @@
 def main():
-    
+    inicio = int ( input("Inicio do intervalo: "))
+    fim = int ( input("Fim do intervalo: "))
+
+    if inicio == fim:
+
+        i = 1
+        print("Tabuada do ", inicio)
+        while i <= 10:
+            print(inicio, "X", i, " =", inicio * i)
+            i += 1
+    elif inicio < fim:
+        while inicio <= fim:
+            
+            i = 1
+
+            print("Tabauda do ", inicio)
+
+            while  i <= 10:
+                print(inicio, "X", i, " =",inicio * i)
+                i += 1
+            inicio += 1
+            print("\n")
+
+    else:
+        while inicio >= fim:
+
+            i = 1
+
+            print("Tabauda do", inicio)
+
+            while i <= 10:
+                print(inicio, "X", i, " =",inicio * i)
+                i += 1
+                
+            inicio -= 1
+            print("\n")
+
 
     return 0
 main()
